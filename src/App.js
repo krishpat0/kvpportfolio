0a57405ec9cae85185fafc0eb45b91be5f96f7bf
krishpat0 <krishpat001800@gmail.com>
2023-08-04 16:29:17 +0530

    create kvp portfolio using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Hey I am Krishna Patil
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          a Software Engineer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="App-main">
+        <h2>About</h2>
+      </main>
     </div>
   );
 }
